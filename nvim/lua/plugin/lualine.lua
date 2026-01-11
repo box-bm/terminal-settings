8322627636e44de489cdeb0a779c182f8864bfbe
box-bm <57267719+box-bm@users.noreply.github.com>
2026-01-10 20:39:42 -0600

    cleanup: removed all about catppuccin and added ignore git folder

--- nvim/lua/plugin/lualine.lua
+++ nvim/lua/plugin/lualine.lua
@@ -1,26 +1,25 @@
 require("lualine").setup({
-	options = {
-		-- theme = "catppuccin",
+  options = {
     theme = "jellybeans",
-		icons_enabled = true,
-		always_divide_middle = true,
-	},
-	sections = {
-		lualine_a = { "mode" },
-		lualine_b = { "branch", "diff", "diagnostics" },
-		lualine_c = { "filename" },
+    icons_enabled = true,
+    always_divide_middle = true,
+  },
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = { "branch", "diff", "diagnostics" },
+    lualine_c = { "filename" },
 
-		lualine_x = { "filetype" },
-		lualine_y = {},
-		lualine_z = { "location" },
-	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
-		lualine_y = {},
-		lualine_z = {},
-	},
-	tabline = {},
+    lualine_x = { "filetype" },
+    lualine_y = {},
+    lualine_z = { "location" },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { "filename" },
+    lualine_x = { "location" },
+    lualine_y = {},
+    lualine_z = {},
+  },
+  tabline = {},
 })
