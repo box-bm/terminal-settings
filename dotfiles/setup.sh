8d5114b26babf08792ba5703b0e59aca0f6c6c95
Brandon Manzo <57267719+box-bm@users.noreply.github.com>
2026-01-31 10:29:40 -0600

    Create setup.sh

---
+++ dotfiles/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+DOTFILES="$HOME/.config/dotfiles"
+BACKUP_DIR="$HOME/.dotfiles_backup"
+
+mkdir -p "$BACKUP_DIR"
+
+link() {
+  src="$1"
+  dest="$2"
+
+  if [ -L "$dest" ]; then
+    echo "Symlink already exists: $dest"
+    return
+  fi
+
+  if [ -e "$dest" ]; then
+    echo "Backing up $dest"
+    mv "$dest" "$BACKUP_DIR/"
+  fi
+
+  ln -s "$src" "$dest"
+  echo "Linked $dest → $src"
+}
+
+# Zsh
+link "$DOTFILES/zsh/zshrc" "$HOME/.zshrc"
