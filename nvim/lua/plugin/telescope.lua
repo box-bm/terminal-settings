e2ca7a1806b6eb5d4be73c4596d3ac970d29fbae
box-bm <57267719+box-bm@users.noreply.github.com>
2026-02-03 23:06:51 -0600

    feat: imrpved lua settings and theme

--- nvim/lua/plugin/telescope.lua
+++ nvim/lua/plugin/telescope.lua
@@ -22,7 +22,7 @@ telescope.setup({
 		layout_strategy = "horizontal",
 		layout_config = {
 			horizontal = {
-				prompt_position = "bottom",
+				prompt_position = "top",
 				preview_width = 0.55,
 				results_width = 0.8,
 			},
--- nvim/lua/plugin/theme.lua
+++ nvim/lua/plugin/theme.lua
@@ -7,12 +7,12 @@ vim.o.background = "dark"
 
 vim.cmd.colorscheme("jellybeans")
 
-vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "CursorLine", { bg = "#1c1c1c" })
-vim.api.nvim_set_hl(0, "Visual", { bg = "#303030" })
-
-vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "LineNr", { fg = "#5f5f5f" })
-vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ffd75f", bold = true })
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+-- vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
+-- vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "NONE" })
+-- vim.api.nvim_set_hl(0, "CursorLine", { bg = "#1c1c1c" })
+-- vim.api.nvim_set_hl(0, "Visual", { bg = "#303030" })
+--
+-- vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
+-- vim.api.nvim_set_hl(0, "LineNr", { fg = "#5f5f5f" })
+-- vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ffd75f", bold = true })
